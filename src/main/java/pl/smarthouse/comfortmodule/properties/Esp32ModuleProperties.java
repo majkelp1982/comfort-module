cf14881b927091a371ff0bdca32cbaec0da83a9d
majkelp1982 <s.pomazanka@gmail.com>
2023-08-05 10:00:10 +0200

    upgrade module firmware version

--- src/main/java/pl/smarthouse/comfortmodule/properties/Esp32ModuleProperties.java
+++ src/main/java/pl/smarthouse/comfortmodule/properties/Esp32ModuleProperties.java
@@ -2,6 +2,6 @@ package pl.smarthouse.comfortmodule.properties;
 
 public class Esp32ModuleProperties {
   // Module specific
-  public static final String FIRMWARE = "20230317.00";
+  public static final String FIRMWARE = "20230805.09";
   public static final String VERSION = "20230318.14";
 }
